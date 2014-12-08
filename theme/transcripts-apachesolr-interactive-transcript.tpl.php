c066449c2e3c6924311de48a93b42943211a560e
Edward Garrett <edward@pinedrop.com>
2014-12-08 01:48:42 +0000

    simplified to use new transcript_ui api

--- theme/transcripts-apachesolr-interactive-transcript.tpl.php
+++ theme/transcripts-apachesolr-interactive-transcript.tpl.php
@@ -1,27 +1,23 @@
 <div>
 	<div class='transcript-player'>
-		<?php print render($transcript_controls); ?>
-		<table>
-			<tr>
-				<td class='video-wrapper'>
-					<?php print $video_tag; ?>
-					<?php print render($video_controls); ?>
-				</td>
-				<td class='transcript-wrapper'>
-					<ul class='nav nav-tabs' role='tablist'>
-						<li class='active'><a href='#transcript-<?php print $trid; ?>' role='tab' data-toggle='tab'>Transcript</a></li>
-						<li><a href='#hits-<?php print $trid; ?>' role='tab' data-toggle='tab'>Search</a></li>
-					</ul>
-					<div class='transcript-content tab-content'>
-						<div class='tab-pane active' id='transcript-<?php print $trid; ?>'>
-							<?php print render($transcript); ?>
-						</div>
-						<div class='tab-pane' id='hits-<?php print $trid; ?>'>
-							<?php print render($hits); ?>
-						</div>
-					</div>
-				</td>
-			</tr>
-		</table>
+		<div class='video-wrapper' data-spy='affix' data-offset-top='100'>
+			<?php print $video_tag; ?>
+			<?php print render($video_controls); ?>
+			<?php print render($transcript_controls); ?>
+		</div>
+		<div class='transcript-wrapper'>
+			<ul class='nav nav-tabs' role='tablist'>
+				<li class='active'><a href='#transcript-<?php print $trid; ?>' role='tab' data-toggle='tab'>Transcript</a></li>
+				<li><a href='#hits-<?php print $trid; ?>' role='tab' data-toggle='tab'>Search</a></li>
+			</ul>
+			<div class='transcript-content tab-content'>
+				<div class='tab-pane active' id='transcript-<?php print $trid; ?>'>
+					<?php print render($transcript); ?>
+				</div>
+				<div class='tab-pane' id='hits-<?php print $trid; ?>'>
+					<?php print render($hits); ?>
+				</div>
+			</div>
+		</div>
 	</div>
 </div>
---
+++ transcripts_apachesolr.api.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @file
+ *   Exposed Hooks
+ */
+
+/**
+ * Redirects from transcript/trid/tcuid to custom URL.
+ *
+ * @param string $transcript
+ *   DB row from table transcripts_apachesolr_transcript.
+ * @param string $fragment
+ *   Tcu fragment identifier, in the form tcu/tcuid.
+ */
+function hook_transcripts_apachesolr_redirect($transcript, $fragment = NULL) {
+	/* redirect to node/nid/transcript#tcu/tcuid */
+
+        $path = 'node/'.$transcript['id'].'/transcript';
+        if ($fragment === NULL) {
+                drupal_goto($path);
+        }
+        else {
+                drupal_goto($path, array('fragment' => $fragment));
+	        }
+}
