3030d8dcb962167c5b50a709df2472a04ac430b7
Edward Garrett <edward@pinedrop.com>
2015-01-10 20:20:25 +0000

    removing bootstrap setting

--- theme/transcripts-apachesolr-interactive-transcript.tpl.php
+++ theme/transcripts-apachesolr-interactive-transcript.tpl.php
@@ -3,21 +3,9 @@
 		<div class='video-wrapper' data-spy='affix' data-offset-top='100'>
 			<?php print $video_tag; ?>
 			<?php print render($transcript_controls); ?>
-			<?php print render($tier_selector); ?>
 		</div>
 		<div class='transcript-wrapper'>
-			<ul class='nav nav-tabs' role='tablist'>
-				<li class='active'><a href='#transcript-<?php print $trid; ?>' role='tab' data-toggle='tab'>Transcript</a></li>
-				<li><a href='#hits-<?php print $trid; ?>' role='tab' data-toggle='tab'>Search</a></li>
-			</ul>
-			<div class='transcript-content tab-content'>
-				<div class='tab-pane active' id='transcript-<?php print $trid; ?>'>
-					<?php print render($transcript); ?>
-				</div>
-				<div class='tab-pane' id='hits-<?php print $trid; ?>'>
-					<?php print render($hits); ?>
-				</div>
-			</div>
+			<?php print render($transcript); ?>
 		</div>
 	</div>
 </div>
