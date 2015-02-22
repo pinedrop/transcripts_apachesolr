d57eb00db5757e3b88da778959f4f69ee2f90aee
Edward Garrett <edward@pinedrop.com>
2015-02-22 19:20:07 +0000

    removing data spy offsets for default interactive transcript view

--- theme/transcripts-apachesolr-interactive-transcript.tpl.php
+++ theme/transcripts-apachesolr-interactive-transcript.tpl.php
@@ -1,6 +1,6 @@
 <div class='transcripts-interactive-transcript'>
     <div class='transcript-player'>
-        <div class='video-wrapper' data-spy='affix' data-offset-top='100'>
+        <div class='video-wrapper'>
             <?php print $video_tag; ?>
             <?php print render($transcript_controls); ?>
         </div>
