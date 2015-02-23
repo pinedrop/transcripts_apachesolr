60de28e207d8e7e5495cc73721a543527f81053f
Edward Garrett <edward@pinedrop.com>
2015-02-23 07:32:55 +0000

    responsive embed experiment

--- theme/transcripts-apachesolr-interactive-transcript.tpl.php
+++ theme/transcripts-apachesolr-interactive-transcript.tpl.php
@@ -1,7 +1,9 @@
 <div class='transcripts-interactive-transcript'>
     <div class='transcript-player'>
         <div class='video-wrapper'>
-            <?php print $video_tag; ?>
+	    <div class="embed-responsive embed-responsive-16by9">
+            	<?php print $video_tag; ?>
+	    </div>
             <?php print render($transcript_controls); ?>
         </div>
         <div class='transcript-wrapper'>
