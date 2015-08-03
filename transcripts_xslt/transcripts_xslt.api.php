f686e0db6ebc315d0d02f231d66d322be40f9324
Edward Garrett <edward@pinedrop.com>
2015-08-03 11:50:09 +0100

    debugging newly inserted node problem

---
+++ transcripts_xslt/transcripts_xslt.api.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ *   Exposed Hooks
+ */
+
+/**
+ * Redirects from transcript/trid/tcuid to custom URL.
+ *
+ * @param array &$params
+ *   Parameters to send to the stylesheet.
+ * @param object $file
+ *   The file that is being transformed.
+ */
+function hook_transcripts_xslt_saxon_params_alter(&$params, $file) {
+
+}
