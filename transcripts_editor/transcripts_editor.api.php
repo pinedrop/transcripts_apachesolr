a253eb0b327d8ce1b42840ad3a440c384eeec9e2
Edward Garrett <edward@pinedrop.com>
2015-09-18 15:03:05 +0100

    added hook to facilitate disabling of transcript editing for specific nodes as necessary

--- transcripts_editor/transcripts_editor.api.php
+++ transcripts_editor/transcripts_editor.api.php
@@ -1,5 +1,14 @@
 <?php
 
+function hook_transcripts_editor_disabled($node) {
+    if ($node->type == 'audio') {
+        return TRUE; //do not allow editing of audio nodes
+    }
+    else {
+        return FALSE;
+    }
+}
+
 function hook_transcripts_editor_exclude_tiers($node)
 {
     //Wylie transliteration is algorithmically derived from Tibetan script, so it should not be edited
